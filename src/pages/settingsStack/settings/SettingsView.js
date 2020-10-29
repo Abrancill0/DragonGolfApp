a8fbeaf6b78ff04f953dfc8a0b17f0cca4b9d2c6
Chuy Lopez <chuy_borra@hotmail.com>
2020-10-29 11:20:20 -0600

    Update SettingsView.js

--- src/pages/settingsStack/settings/SettingsView.js
+++ src/pages/settingsStack/settings/SettingsView.js
@@ -1487,7 +1487,7 @@ class SettingsView extends Component {
                     console.warn('Results', results);
                     if (results.rowsAffected > 0) {
                       console.warn("OK")
-                    } else alert('Updation Failed');
+                    } else console.warn('Updation Failed');
                   }
                 );
               });
@@ -1921,7 +1921,7 @@ class SettingsView extends Component {
                 this.setState({
                   btnAct: true
                 })
-                } else alert('Updation Failed');
+                } else console.warn('Updation Failed');
               }
             );
             console.warn(tx)
